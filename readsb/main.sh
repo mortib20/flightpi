c576119080947ac0934d97fabaa75972bfc060cb
mortib20 <martin.paul-3277@web.de>
2023-02-11 22:27:02 +0100

    entrypoint

--- readsb/main.sh
+++
@@ -1,6 +0,0 @@
-readsb --device-type=rtlsdr --device $READSB_DEVICE --gain $READSB_GAIN --ppm 0 \
---lat $READSB_LAT --lon $READSB_LON \
---max-range 450 --write-json-every 1 \
---net --net-heartbeat 60 --net-ro-size 1250 --net-ro-interval 0.05 \
---net-ri-port 30001 --net-ro-port 30002 --net-sbs-port 30003 --net-bi-port 30004,30104 --net-bo-port 30005 \
---json-location-accuracy 2 --range-outline-hours 24 --write-json /run/readsb
