42de97fc09dfa9c92a87fe282dd1f795d84ec43a
mortib20 <martin.paul-3277@web.de>
2023-02-12 13:00:06 +0100

    added fixed and other stuff to rb24feed

--- fr24feed/download.sh
+++ fr24feed/download.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
-MACHINE=`uname -m`
+ARCH=`arch`
 
-echo "Machine Type: $MACHINE"
+echo "Arch Type: $ARCH"
 
-if [ "$MACHINE" == "x86_64" ]; then
+if [ "$ARCH" == "x86_64" ]; then
     wget -c https://repo-feed.flightradar24.com/linux_x86_64_binaries/fr24feed_1.0.34-0_amd64.tgz -O - | tar -xz
     mv fr24feed_amd64 fr24feed
 fi
 
-if [ "$MACHINE" == "aarch64" ]; then
+if [ "$ARCH" == "x86" ]; then
+    wget -c https://repo-feed.flightradar24.com/linux_x86_binaries/fr24feed_1.0.34-0_i386.tgz -O - | tar -xz
+    mv fr24feed_i386 fr24feed
+fi
+
+if [ "$ARCH" == "aarch64" ]; then
     wget -c https://repo-feed.flightradar24.com/rpi_binaries/fr24feed_1.0.34-0_armhf.tgz -O - | tar -xz
     mv fr24feed_armhf fr24feed
 fi
+
+mv fr24feed/fr24feed /usr/local/bin
+rm -rf fr24feed
